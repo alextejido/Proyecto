7dad5acc3185604eb55ad2e8ef8097f174289017
Alex <Alex@Alex-PC>
2017-06-04 20:21:36 +0200

    en proceso

---
+++ src/proyecto_tercer_trimestre/Proyecto_Tercer_Trimestre.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_tercer_trimestre;
+
+/**
+ *
+ * @author artur
+ */
+public class Proyecto_Tercer_Trimestre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //necesita un list(se llama asi en la palette) que es donde lleva el resultado
+//        necesita un boton de siguiente o aceptar 
+//        si quieres un titulo tambien
+//        un campo que seria precio(que no es por desanimarte pero nos olvifdamos de meterlo en la tabla vuelos, pero ya te eplicare mañana lo que tengo pensado) 
+//        
+    }
+    
+}
